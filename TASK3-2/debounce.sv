793d48ad10a965746c2dcb5e06e46fdaeb48fca5
AzurMe <kuangmingyuan0819@outlook.com>
2025-10-25 11:53:04 +0800

    update t3-2

--- TASK3-2/debounce.sv
+++ TASK3-2/debounce.sv
@@ -3,52 +3,57 @@
  * 功能：按键消抖，并产生单周期脉冲
  */
 module debouncer (
-    input  logic clk,
-    input  logic rst_n,
-    input  logic btn_in,    // 原始按键输入
-    output logic btn_out,   // 消抖后的电平
-    output logic btn_pulse  // 消抖后的上升沿脉冲
+    input       clk,
+    input       rst_n,
+    input       btn_in,
+    output reg  btn_out,
+    output      btn_pulse,
 );
 
-    // 20ms 消抖时间 @ 50MHz
-    localparam DEBOUNCE_MAX =  1000; // 1_000_000
-    
-    logic [$clog2(DEBOUNCE_MAX):0] cnt;
-    logic btn_sync1, btn_sync2;
-    logic btn_stable;
-    logic btn_prev;
+parameter N = 20 ;           // debounce timer bitwidth (Need Calculation)
+parameter FREQ = 50;         //model clock :Mhz
+parameter MAX_TIME = 20;     //ms
+localparam TIMER_MAX_VAL =   MAX_TIME * 1000 * FREQ;
 
-    assign btn_out = btn_stable;
-    assign btn_pulse = btn_stable & ~btn_prev;
-
-    always_ff @(posedge clk or negedge rst_n) begin
+    reg Q1, Q2;
+    always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            cnt <= 0;
-            btn_sync1 <= 1'b0;
-            btn_sync2 <= 1'b0;
-            btn_stable <= 1'b0;
-            btn_prev <= 1'b0;
+            Q1 <= 1'b1;
+            Q2 <= 1'b1;
         end else begin
-            // 同步
-            btn_sync1 <= btn_in;
-            btn_sync2 <= btn_sync1;
-            
-            // 状态改变，则计数器复位
-            if (btn_sync2 != btn_stable) begin
-                cnt <= 0;
-            end
-            // 计数器未满，继续计数
-            else if (cnt != DEBOUNCE_MAX) begin
-                cnt <= cnt + 1;
-            end
-            // 计数器已满，且状态稳定，更新输出
-            else begin
-                btn_stable <= btn_sync2;
-            end
-            
-            // 记录前一个周期的稳定状态
-            btn_prev <= btn_stable;
+            Q1 <= btn_in;
+            Q2 <= Q1;
         end
     end
+
+    assign button_diff = Q1 ^ Q2;
+
+    reg [N-1:0] cnt;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            cnt <= 0;
+        end else if (button_diff == 1) begin
+            cnt <= 0;
+        end else if (cnt == TIMER_MAX_VAL) begin
+            cnt <= cnt;
+        end else begin
+            cnt <= cnt + 1;
+        end
+    end
+
+    reg btn_out_temp1, btn_out_temp2;
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            btn_out <= 1'b1;
+            btn_out_temp1 <= 1'b1;
+            btn_out_temp2 <= 1'b1;
+        end else if (cnt == TIMER_MAX_VAL) begin
+            btn_out <= Q2;
+            btn_out_temp1 <= Q2;
+            btn_out_temp2 <= btn_out_temp1;
+        end
+    end
+
+    assign btn_pulse = (btn_out_temp1 == 1'b1) && (btn_out_temp2 == 1'b0);
 
 endmodule
